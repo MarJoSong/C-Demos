5c564de96a3bee239f518514bded82589df7821e
Huahua <769200142@qq.com>
2020-11-05 17:44:30 +0800

    fix no client info

--- SocketForLinux/server.c
+++ SocketForLinux/server.c
@@ -11,7 +11,7 @@
 
 typedef struct __st_deal_conn {
     int *connectfd;
-    struct sockaddr_in *cliaddr;
+    struct sockaddr_in *p_cliaddr;
 } st_deal_conn;
 
 // 线程的运行函数
@@ -20,10 +20,10 @@ void* deal_connection(void* args)
     int n;
     char ip[MAXLINE];
     char buf[MAXLINE];
-    st_deal_conn *deal_conn_st = (st_deal_conn *)args;
+    st_deal_conn *p_deal_conn_st = (st_deal_conn *)args;
 
     while (1) {
-        n = read(*deal_conn_st->connectfd, buf, MAXLINE);
+        n = read(*p_deal_conn_st->connectfd, buf, MAXLINE);
         /*
          * char *inet_ntoa(struct in_addr in);
          * 将Internet主机地址(网络字节序)转换为IPv4点状十进制符号的字符串。
@@ -34,16 +34,16 @@ void* deal_connection(void* args)
          */
         if (n<=0) {
             printf("Host [%s] lost connection.\n",
-                   inet_ntop(PF_INET, &deal_conn_st->cliaddr->sin_addr, ip, MAXLINE));
+                   inet_ntop(PF_INET, &p_deal_conn_st->p_cliaddr->sin_addr, ip, MAXLINE));
             break;
         }
         printf("Read len [%d] bytes from host:[%s] on port:[%d]\n", n,
-               inet_ntop(PF_INET, &deal_conn_st->cliaddr->sin_addr, ip, MAXLINE),
-               ntohs(deal_conn_st->cliaddr->sin_port));
+               inet_ntop(PF_INET, &p_deal_conn_st->p_cliaddr->sin_addr, ip, MAXLINE),
+               ntohs(p_deal_conn_st->p_cliaddr->sin_port));
         for (int i = 0; i < n; i++) {
             buf[i] = toupper(buf[i]);
         }
-        write(*deal_conn_st->connectfd, buf, n);
+        write(*p_deal_conn_st->connectfd, buf, n);
     }
 
     return NULL;
@@ -75,31 +75,33 @@ int main(int argc, char *argv[])
 
     int i=0;
     pthread_t deal_threads[NUM_THREADS];
-    st_deal_conn *deal_conn_st;
+    struct sockaddr_in *p_cliaddr;
+    socklen_t *p_cliaddr_len;
+    st_deal_conn *p_deal_conn_st;
+
     printf("Host:[%s] accepting connections on port:[%d]\n",
            inet_ntoa(servaddr.sin_addr), ntohs(servaddr.sin_port));
     while (i<NUM_THREADS) {
         int *connectfd = (int *)malloc(sizeof(int));
-        socklen_t *cliaddr_len = (socklen_t *)malloc(sizeof(socklen_t));
-        struct sockaddr_in *cliaddr = (struct sockaddr_in *)malloc(sizeof(struct sockaddr_in));
+        p_cliaddr_len = (socklen_t *)malloc(sizeof(socklen_t));
+        p_cliaddr = (struct sockaddr_in *)malloc(sizeof(struct sockaddr_in));
+        //初始化及socklen_t长度赋初值是必须的，否则客户端信息取不到
+        *p_cliaddr_len = sizeof(struct sockaddr_in);
+        bzero(p_cliaddr, *p_cliaddr_len);
 
-        *connectfd = accept(listenfd, (struct sockaddr *)cliaddr, cliaddr_len);
-        printf("accpet from host:[%s] on port:[%d]\n",
-               inet_ntoa(cliaddr->sin_addr), ntohs(cliaddr->sin_port));
-        deal_conn_st = (st_deal_conn *)malloc(sizeof(st_deal_conn));
-        deal_conn_st->connectfd = connectfd;
-        deal_conn_st->cliaddr = cliaddr;
-        printf("accpet from host:[%s] on port:[%d]\n",
-               inet_ntoa(deal_conn_st->cliaddr->sin_addr),
-               ntohs(deal_conn_st->cliaddr->sin_port));
-        if (pthread_create(&deal_threads[i++], NULL, deal_connection, deal_conn_st)) {
+        *connectfd = accept(listenfd, (struct sockaddr *)p_cliaddr, p_cliaddr_len);
+        p_deal_conn_st = (st_deal_conn *)malloc(sizeof(st_deal_conn));
+        p_deal_conn_st->connectfd = connectfd;
+        p_deal_conn_st->p_cliaddr = p_cliaddr;
+
+        if (pthread_create(&deal_threads[i++], NULL, deal_connection, p_deal_conn_st)) {
             printf("create deal thread failed.\n");
 
             close(listenfd);
             exit(1);
         } else {
             printf("Host:[%s] request connection on port:[%d].\n",
-                   inet_ntoa(cliaddr->sin_addr), ntohs(cliaddr->sin_port));
+                   inet_ntoa(p_cliaddr->sin_addr), ntohs(p_cliaddr->sin_port));
         }
     }
 
